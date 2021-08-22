0e24b62341559354a98ee8cd83b748d5d12ced8d
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-22 22:44:31 +0700

    Add favicon

--- partials/head.php
+++ partials/head.php
@@ -6,4 +6,10 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="../icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png">
+<link rel="manifest" href="../icons/site.webmanifest">
+<!-- CSS -->
 <link rel="stylesheet" href="./css/main.css">
